e9472d648a9ff9373433bd97e11edb5b1d10c807
JabedH <jabedhtusar@gmail.com>
2022-06-30 20:03:54 +0600

    added calender

--- src/Components/Calendar/Calendar.js
+++ src/Components/Calendar/Calendar.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
 
 const Calendar = () => {
+  const [selected, setSelected] = useState(new Date());
   return (
-    <div>
+    <div className="h-[500px]">
       <h1>Calendar</h1>
+      <div className=" flex justify-center items-center">
+        <DayPicker mode="single" selected={selected} onSelect={setSelected} />
+      </div>
     </div>
   );
 };
