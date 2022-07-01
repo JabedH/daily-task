26a8513330b0fcdd699cc35cee25999b79c5006b
JabedH <jabedhtusar@gmail.com>
2022-07-01 20:08:38 +0600

    shown todo list on home section

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ToDo from "../Todo/ToDo";
 
 const Home = () => {
+  const [todo, setTodo] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/addlist")
+      .then((res) => res.json())
+      .then((data) => setTodo(data.slice(0, 4)));
+  });
   return (
     <div className=" drawer ">
-      <h1>home</h1>
+      <div class="overflow-x-auto">
+        <h1 className=" font-bold my-5">My Todo List</h1>
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Date</th>
+              <th>Place</th>
+            </tr>
+          </thead>
+          <tbody>
+            {todo.map((list) => (
+              <tr>
+                <td>{list.title}</td>
+                <td>{list.date}</td>
+                <td>{list.place}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
